b9db62ac570f19e22ac3e3c8b7815588d6856854
Vitor Guedes <vittorbassdev@gmail.com>
2020-07-04 18:41:54 -0300

    Implementação | Items | Pontos Cadastro de Pontos Listagem de Items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,57 @@
-import express from 'express';
-
+import express, { request, response } from 'express';
+import Knex from './database/connection';
+ 
 const routes = express.Router();
 
-routes.get('/', (request, response) =>{
-    return response.json({message: 'Hello Word 02'});
+routes.get('/items', async (request, response) =>{
+    const items = await Knex('items').select('*');
+    
+    const serializedItems = items.map(item =>{
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
+    })
+    
+    return response.json(serializedItems);
 });
 
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const trx = await Knex.transaction();
+    
+    const insertIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertIds[0]
+    const pointItems = items.map((item_id: number) => {
+        return{
+            item_id,
+            point_id,
+        }
+    });
+    await trx('point_items').insert(pointItems)
+
+    return response.json({sucess: true});
+});
 export default routes;
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,15 @@
 import express, { request, response } from 'express'; 
 // AS ROTAS ESTÃO SENDO IMPORTADAS DE DENTRO DO ARQUIVO ROUTES.TS
 import routes from './routes';
+import path from 'path';
 
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+// SETAR ROTA PARAS AS IMAGENS DOS ITENS
+app.use("/uploads", express.static(path.resolve(__dirname, '..', 'uploads')))
 
 // Setar Porta para execução 
 app.listen(3333); 
