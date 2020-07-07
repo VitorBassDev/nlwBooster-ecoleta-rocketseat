125e98ce178f57d8dd0b94bfcb376301c6082806
Vitor Guedes <vittorbassdev@gmail.com>
2020-07-06 21:07:31 -0300

    Criando Inserção e Buscas no PointsController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,27 +1,17 @@
-import express, { request, response } from 'express';
-import Knex from './database/connection';
+import express from 'express';
  
-import PointsController from  './controllers/PointsController';
+import PointsController from './controllers/PointsController';
+import ItemsController  from './controllers/ItemsController';
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) =>{
-    const items = await Knex('items').select('*');
-    
-    const serializedItems = items.map(item =>{
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        }
-    })
-    
-    return response.json(serializedItems);
-});
-
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
 
