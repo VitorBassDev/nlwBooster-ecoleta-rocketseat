06375e7b2a87a14ef7665c8857778513a5b67d65
Vitor Guedes <vittorbassdev@gmail.com>
2020-07-21 11:15:34 -0300

    Implementação da página de detalhes

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,15 +1,59 @@
 import React from 'react';
-import {View} from 'react-native';
-
+import {View, StyleSheet, Image, Text, TouchableOpacity, SafeAreaView} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {Feather as Icon, FontAwesome} from '@expo/vector-icons'
+import {RectButton} from 'react-native-gesture-handler'
 
 const Detail = () =>{
-  return <View/>
+  const navigation = useNavigation();
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+        <Icon name="arrow-left" size={20} color="#34cb79"/>
+        </TouchableOpacity>
+
+        <Image 
+          style={styles.pointImage}
+          source={{uri: 'https://images.unsplash.com/photo-1593870103832-d9359e509a00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60-teste'}}  
+        />
+        <Text style={styles.pointName}> Mercadão do João</Text>
+        <Text style={styles.pointItems}> Lâmpada, Oléo</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}> Endereço  </Text>
+          <Text style={styles.addressContent}> Rio do Sul | SC  </Text>
+        </View>
+      </View>
+      <View style={styles.footer}> 
+
+        <RectButton 
+          style={styles.button} 
+          onPress={() => {}}>
+            <FontAwesome name="whatsapp" size={20} color="#FFF"/>
+            <Text style={styles.buttonText}> Whatsapp</Text>
+        </RectButton>
+
+        <RectButton 
+          style={styles.button} 
+          onPress={() => {}}>
+            <Icon name="mail" size={20} color="#FFF"/>
+            <Text style={styles.buttonText}> E-mail</Text>
+        </RectButton>
+
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default Detail;
 
 
-/**
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -61,6 +105,7 @@ const styles = StyleSheet.create({
     borderTopWidth: StyleSheet.hairlineWidth,
     borderColor: '#999',
     paddingVertical: 20,
+    paddingBottom: 5,
     paddingHorizontal: 32,
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -84,4 +129,3 @@ const styles = StyleSheet.create({
   },
 });
 
-*/
